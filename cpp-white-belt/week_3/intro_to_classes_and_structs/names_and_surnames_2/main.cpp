1abe5cb4463dc0607806645c78bc0c597a07323e
NoSpooksAllowed <hersgori4@protonmail.com>
2020-11-24 23:42:07 +0300

    corrected space intendation

--- cpp-white-belt/week_3/intro_to_classes_and_structs/names_and_surnames_2/main.cpp
+++ cpp-white-belt/week_3/intro_to_classes_and_structs/names_and_surnames_2/main.cpp
@@ -108,18 +108,18 @@ private:
 };
 
 int main() {
-  Person person;
+    Person person;
 
-  person.ChangeFirstName(1965, "Polina");
-  person.ChangeFirstName(1965, "Appolinaria");
+    person.ChangeFirstName(1965, "Polina");
+    person.ChangeFirstName(1965, "Appolinaria");
 
-  person.ChangeLastName(1965, "Sergeeva");
-  person.ChangeLastName(1965, "Volkova");
-  person.ChangeLastName(1965, "Volkova-Sergeeva");
+    person.ChangeLastName(1965, "Sergeeva");
+    person.ChangeLastName(1965, "Volkova");
+    person.ChangeLastName(1965, "Volkova-Sergeeva");
 
-  for (int year : {1964, 1965, 1966}) {
-    cout << person.GetFullNameWithHistory(year) << endl;
-  }
+    for (int year : {1964, 1965, 1966}) {
+        cout << person.GetFullNameWithHistory(year) << endl;
+    }
 
-  return 0;
+    return 0;
 }
