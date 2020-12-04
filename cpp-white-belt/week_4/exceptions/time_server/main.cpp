974cf2cd87eed0cdef867a33194318b18b4dca24
NoSpooksAllowed <hersgori4@protonmail.com>
2020-12-04 21:20:49 +0300

    finished time server assignment

---
+++ cpp-white-belt/week_4/exceptions/time_server/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <exception>
+#include <string>
+using namespace std;
+
+string AskTimeServer() {
+
+    throw invalid_argument("invalid argument");
+}
+
+class TimeServer {
+public:
+    string GetCurrentTime() {
+        try {
+            last_fetched_time = AskTimeServer();
+
+            return last_fetched_time;
+        } catch (system_error& s) {
+            return last_fetched_time;
+        } 
+        
+    }
+
+private:
+    string last_fetched_time = "00:00:00";
+};
+
+int main() {
+    TimeServer ts;
+    try {
+        cout << ts.GetCurrentTime() << endl;
+    } catch (exception& e) {
+        cout << "Exception got: " << e.what() << endl;
+    }
+    return 0;
+}
