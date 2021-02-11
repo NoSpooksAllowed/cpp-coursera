12f069c4dcade399e52adfd08905f28696656616
spook <hersgori4@protonmail.com>
2021-02-11 22:06:30 +0300

    finished find starts with string assignment

---
+++ cpp-yellow-belt/week_4/queue_deque_searching_algorithms/find_starts_with_string/find_starts_with.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <utility>
+#include <algorithm>
+#include <string>
+
+template <typename RandomIt>
+std::pair<RandomIt, RandomIt> FindStartsWith(
+    RandomIt range_begin, 
+    RandomIt range_end,
+    std::string prefix
+) {
+    return std::equal_range(range_begin, range_end, prefix,
+        [&prefix](const std::string& a, const std::string& b) {
+            return a.compare(0, prefix.length(), b.substr(0, prefix.length())) < 0;
+        }
+    );
+}
---
+++ cpp-yellow-belt/week_4/queue_deque_searching_algorithms/find_starts_with_string/main.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+#include <string>
+#include <iostream>
+
+#include "find_starts_with.h"
+
+int main() {
+    const std::vector<std::string> sorted_strings = {"moscow", "motovilikha", "murmansk"};
+
+    const auto mo_result =
+        FindStartsWith(begin(sorted_strings), end(sorted_strings), "mo");
+    for (auto it = mo_result.first; it != mo_result.second; ++it) {
+        std::cout << *it << " ";
+    }
+    std::cout << std::endl;
+
+    const auto mt_result =
+        FindStartsWith(begin(sorted_strings), end(sorted_strings), "mt");
+    std::cout << (mt_result.first - begin(sorted_strings)) << " " <<
+        (mt_result.second - begin(sorted_strings)) << std::endl;
+
+    const auto na_result =
+        FindStartsWith(begin(sorted_strings), end(sorted_strings), "na");
+    std::cout << (na_result.first - begin(sorted_strings)) << " " <<
+      (na_result.second - begin(sorted_strings)) << std::endl;
+
+    return 0;
+}
